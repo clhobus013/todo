4e6e3aeac4026c518c2156cb97c23ee176383a80
claudia <clauu013@gmail.com>
2024-12-09 23:16:19 -0300

    Adiciona input nova tarefa

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,6 +7,17 @@ export function Home() {
             <View style={styles.topBar}>
                 <Image source={require("../../../assets/Logo.png")}  style={styles.logo} ></Image>
             </View>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Adicione uma nova tarefa"
+                    placeholderTextColor='#808080'
+                />
+                <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.textButton}>+</Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -9,4 +9,30 @@ export const styles = StyleSheet.create({
         height: 47,
         width: 170,
     },
+    input: {
+        flex: 1,
+        color: '#D9D9D9',
+        backgroundColor: '#333333',
+        borderRadius: 5,
+        padding: 20,
+        fontSize: 16,
+        margin: 5
+    },
+    container: {
+        width: '100%',
+        flexDirection: "row"
+    },
+    addButton: {
+        backgroundColor: '#1E6F9F',
+        width: 50,
+        height: 50,
+        borderRadius: 5,
+        justifyContent: 'center',
+        margin: 5
+    },
+    textButton: {
+       fontSize: 16,
+       color: '#fff',
+       textAlign: 'center'
+   }
 })
